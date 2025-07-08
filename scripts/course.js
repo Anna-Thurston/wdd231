151a1db5191b367d847167fc1901d51ac501319f
annathurston <sfcakester@gmail.com>
2025-07-07 20:48:26 -0500

    Added course list

---
+++ scripts/course.js
@@ -0,0 +1,44 @@
+const courses = [
+    {
+        courseId: 'CSE 110',
+        courseName: 'Introduction to Problem Solving and Programming',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'CSE 111',
+        courseName: 'Programming with Functions',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'WDD 130',
+        courseName: 'Web Fundamentals',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'WDD 131',
+        courseName: 'Dynamic Web Fundamentals',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'CSE 210',
+        courseName: 'Programming with Classes',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'CSE 121b',
+        courseName: 'JavaScript Language',
+        credit: 2,
+        completed: true
+    },
+    {
+        courseId: 'WDD 231',
+        courseName: 'Front-end Web Development I',
+        credit: 2,
+        completed: false
+    }
+];
