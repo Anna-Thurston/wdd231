c9c715cd6483fe3a421242729643dce156d5a984
annathurston <sfcakester@gmail.com>
2025-07-22 17:33:33 -0500

    Added business card template logic and toggle view functionality

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,9 +1,19 @@
 const toggleButton = document.getElementById("menu-toggle");
 const navMenu = document.getElementById("nav-menu");
+const gridBtn = document.getElementById("gridBtn");
+const listBtn = document.getElementById("listBtn");
+const directory = document.getElementById("directory");
 
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+gridBtn.addEventListener("click", () => {
+    directory.classList.add("grid-view");
+    directory.classList.remove("list-view");
+});
+
+listBtn.addEventListener("click", () => {
+    directory.classList.add("list-view");
+    directory.classList.remove("grid-view");
+});
   
 toggleButton.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
+    navMenu.classList.toggle("show");
 });
