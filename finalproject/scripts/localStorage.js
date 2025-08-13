f5f07a6998c675c73071e6ea87d409f8cf45c3be
annathurston <sfcakester@gmail.com>
2025-08-12 19:07:06 -0500

    Added local storage helpers

---
+++ finalproject/scripts/localStorage.js
@@ -0,0 +1,9 @@
+// localStorage.js
+export function saveToLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function loadFromLocalStorage(key) {
+  const value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : null;
+}
